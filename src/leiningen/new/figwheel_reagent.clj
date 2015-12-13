7675be5f515cdd1a25358d4b394be1dccc83d0b8
Davide Taviani <helios89@gmail.com>
2015-12-13 12:30:51 +0100

    Add README and .gitignore

--- src/leiningen/new/figwheel_reagent.clj
+++ src/leiningen/new/figwheel_reagent.clj
@@ -8,11 +8,14 @@
   (let [data {:name name
               :ns-name (sanitize-ns name)
               :sanitized (name-to-path name)}]
-    (->files data ["test/clj/{{sanitized}}/core_test.clj" (render "core_test.clj" data)]
+    (->files data
+             ["test/clj/{{sanitized}}/core_test.clj" (render "core_test.clj" data)]
              ["dev/cljs/{{sanitized}}/dev.cljs" (render "dev.cljs" data)]
              ["src/clj/{{sanitized}}/core.clj" (render "core.clj" data)]
              ["src/clj/{{sanitized}}/server.clj" (render "server.clj" data)]
              ["project.clj" (render "project.clj" data)]
+             ["README.md" (render "README.md" data)]
+             [".gitignore" (render ".gitignore")]
              ["dev/clj/user.clj" (render "user.clj" data)]
              ["src/cljs/{{sanitized}}/core.cljs" (render "core.cljs" data)]
              ["resources/log4j.properties" (render "log4j.properties")]
---
+++ src/leiningen/new/figwheel_reagent/README.md
@@ -0,0 +1,16 @@
+# {{name}}
+
+## Run in development mode
+
+- Start a repl with `lein repl`
+- run `(go)`, this will start figwheel, the server on port 8080, and other bootstrapping stuff
+- If you want a cljs repl, run `cljs-repl` from the `user` namespace
+- Visit `http://localhost:8080`
+
+## Run in production mode
+    lein uberjar
+
+## License
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
